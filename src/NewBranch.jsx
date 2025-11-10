1180e6fb7b8cfb6697d9c7e009d1944e423dd8a2
Ankit Lekharajani <ankitlekharajani752@gmail.com>
2025-11-10 12:08:51 +0530

    First commit from the new branch

---
+++ src/NewBranch.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react"
+
+const NewBranch = () =>{
+  const[data,setData]=useState([1,2,3]);
+
+  useEffect(()=>{
+    
+  })
+  
+  return(
+    <ReactFragment>
+    {
+     <h1>This is the app.jsx</h1>
+    }
+    </ReactFragment>
+  )
+  
+}
